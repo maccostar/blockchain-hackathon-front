334c595fe103caebe5fcc7a36bf8ac5b8a0d9f13
kohei-tcshingai <kohei.shingai@tcs.com>
2019-02-14 07:26:04 +0900

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StudentComponent } from './page/student/student.component';
+import { TeacherComponent } from './page/teacher/teacher.component';
+import { ParentComponent } from './page/parent/parent.component';
+import { ExternalComponent } from './page/external/external.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    TeacherComponent,
+    ParentComponent,
+    ExternalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
