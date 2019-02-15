0102508d03af80ee6bee9153c15dc719f220ec4f
kohei.shingai <kohei.shingai@tcs.com>
2019-02-15 21:37:17 +0900

    update

--- src/app/page/student/student.component.ts
+++ src/app/page/student/student.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
+  private userName : string = "Student";
+  private point : string = "30";
+  private GET_TOKEN : string = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DC2G&redirect_uri=http%3A%2F%2Flocalhost&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800sss";
+
   constructor() { }
 
   ngOnInit() {
+    if (window.localStorage.getItem('token')) {
+        alert('welcome');
+    } else {
+      if(window.location.hash) {
+        let hash = window.location.hash.split('&');
+        let filtered = hash.find(function(element, index, array) {
+          return (element.indexOf('access_token') != -1);
+        });
+        if (filtered) { 
+          window.localStorage.setItem('token', filtered.split('=')[1]);
+          window.location.href = "/";
+        } else {
+          window.location.href = this.GET_TOKEN;
+        }
+      } else {
+        window.location.href = this.GET_TOKEN;
+      }
+    }
   }
-
 }
