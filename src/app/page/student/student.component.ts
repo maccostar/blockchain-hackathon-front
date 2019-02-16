d09db6237d67af7b543c43e9a5bbdb6d40ab10ee
EC2 Default User <ec2-user@ip-172-31-21-208.ap-northeast-1.compute.internal>
2019-02-16 02:32:38 +0000

    update

--- src/app/page/student/student.component.ts
+++ src/app/page/student/student.component.ts
@@ -27,6 +27,7 @@ export class StudentComponent implements OnInit {
   private steps : string = "-";
   private GET_TOKEN : string = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22DC2G&redirect_uri=http%3A%2F%2Flocalhost&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800sss";
 
+
   constructor(private commonService: CommonService) { }
 
   ngOnInit() {
